3db3463be2a02e440708e22a735bb3064ffd3dbf
SND\fehaar_cp <SND\fehaar_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2008-01-06 08:51:33 +0000

    #231: New port. Functions working.
    git-svn-id: https://xnatweener.svn.codeplex.com/svn@1140 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

--- XNATweener/Back.cs
+++ XNATweener/Back.cs
@@ -16,16 +16,14 @@ namespace XNATweener
             return c * ((t = t / d - 1) * t * ((1.70158f + 1) * t + 1.70158f) + 1) + b;
         }
 
-        /// NOTE: This method is not really working...
         public static float EaseInOut(float t, float b, float c, float d)
         {
-            float s = 1.70158f * 1.525f;
+            float s = 1.70158f;
             if ((t /= d / 2) < 1)
             {
-                return c / 2 * (t * t * ((s + 1) * t - 1.70158f)) + b;
+                return c / 2 * (t * t * (((s *= (1.525f)) + 1) * t - s)) + b;
             }
-            t -= 2;
-            return c / 2 * (t * t * ((s + 1) * t + 1.70158f) + 2) + b;
+            return c / 2 * ((t -= 2) * t * (((s *= (1.525f)) + 1) * t + s) + 2) + b;
         }
     }
 }
--- XNATweener/Elastic.cs
+++ XNATweener/Elastic.cs
@@ -36,24 +36,24 @@ namespace XNATweener
 		    return (float)(c * Math.Pow(2,-10*t) * Math.Sin((t*d-s)*(2*Math.PI)/p ) + c + b);
 	    }
 
-        /// NOTE: This method is not really working...
         public static float EaseInOut(float t, float b, float c, float d)
         {
-		    if (t==0) 
+            if (t == 0)
             {
                 return b;
             }
-            if ((t /= d/2) == 2) 
+            if ((t /= d / 2) == 2)
             {
-                return b+c; 
+                return b + c;
             }
             float p = d * (.3f * 1.5f);
+            float a = c;
             float s = p / 4;
             if (t < 1)
             {
-                return -.5f * (float)(c * Math.Pow(2, 10 * (t -= 1)) * Math.Sin((t * d - s) * (2 * Math.PI) / p)) + b;
+                return -.5f * (float)(a * Math.Pow(2, 10 * (t -= 1)) * Math.Sin((t * d - s) * (2 * Math.PI) / p)) + b;
             }
-		    return c * (float)(Math.Pow(2,-10*(t-=1)) * Math.Sin((t*d-s) * (2*Math.PI) / p) * .5 + c + b);
+		    return (float)(a * Math.Pow(2,-10*(t-=1)) * Math.Sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b);
 	    }
     }
 }
